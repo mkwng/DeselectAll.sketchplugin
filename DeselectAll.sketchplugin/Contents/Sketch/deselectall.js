0520fb9d6f68dd1464126b8f12794942866145b6
Michael Wang <hello@mkwng.com>
2017-11-13 09:56:12 -0800

    First commit

---
+++ DeselectAll.sketchplugin/Contents/Sketch/deselectall.js
@@ -0,0 +1,11 @@
+
+
+function onRun(context) {
+
+    var sketch = context.api();
+    var document = sketch.selectedDocument;
+    var selection = document.selectedLayers;
+
+    selection.clear();
+    sketch.message("Deselected all layers");
+};
